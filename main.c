3007413c5ebc8e67f6273f3d612a797dd54dcd45
Jacob de Witte <c3329657@uon.edu.au>
2019-04-09 03:42:30 +0000

    testing commit

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+
+
 int main (){
-  
+  int temp, i;
+  int k = 6;
+  char I[65];
+  for (i=65; i<91; i++){
+  temp = i-65;
+  temp = (temp + k) %26;
+  printf("%c %d, %c\n", i, temp, temp+65);
+  }
   return 0;
 }
+
